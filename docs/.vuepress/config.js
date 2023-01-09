8b3626ae5c1616f2cc487ca1e54fb18ff5449a1c
Yang_qwq <yang_qwq@qq.com>
2023-01-09 09:14:13 +0800

    规范文档格式

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,23 +1,23 @@
 module.exports = {
     title: '方块元服务器',
-    description:'玩家文档',
+    description: '玩家文档',
     head: [
-      [
-          'script', // js 文件
-          { src: '/js/main.js' }
-      ]
-  ],
+        [
+            'script', // js 文件
+            { src: '/js/main.js' }
+        ]
+    ],
     themeConfig: {
-      nav: [
-        { text: '主页', link: '/' },
-        { text: '指南', link: '/zh/guide/' },
-        { text: '问问题', link: '/zh/questions/'},
-        { text: '规则', link: '/zh/rules/'},
-        { text: `定价`, link: '/zh/economic/'}
-      ],
-      
-      sidebar: 'auto',
-      search: true,
-      searchMaxSuggestions: 10
+        nav: [
+            { text: '主页', link: '/' },
+            { text: '指南', link: '/zh/guide/' },
+            { text: '问问题', link: '/zh/questions/' },
+            { text: '规则', link: '/zh/rules/' },
+            { text: `定价`, link: '/zh/economic/' }
+        ],
+
+        sidebar: 'auto',
+        search: true,
+        searchMaxSuggestions: 10
     }
-  }
+}
--- docs/README.md
+++ docs/README.md
@@ -3,7 +3,7 @@ home: true
 heroImage: ./logo.jpg
 heroText: 欢迎查看方块元玩家文档
 tagline: 如有问题请提交至bmssservice@0525.fun
-actionText: 查看文档 →
+actionText: 查看文档
 actionLink: ./zh/guide/
 features:
 - title: 我们拥有负责任的管理组
@@ -14,6 +14,7 @@ features:
   details: 让玩家不花一分钱,体验与众不同的Minecraft服务器
 #footer: MIT Licensed | Copyright © 2018-present Evan You
 ---
+
 ::: slot footer
 MIT Licensed | 方块元服务器 | Copyright © 2023-present [服务器官网](https://ais.bytemetasg.top/)  
 ![公安联网备案标识](policesign.png)[鲁公网安备 37020202000xxx号(公安联网备案办理中)](https://www.beian.gov.cn/)
