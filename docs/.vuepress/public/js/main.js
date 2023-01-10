4f066e6aeba42e31fa03501da27f2a4c3358b5f4
Yang <yang_qwq@qq.com>
2023-01-10 17:19:37 +0800

    Skin plugin docs (#1)

--- docs/.vuepress/public/js/main.js
+++ docs/.vuepress/public/js/main.js
@@ -1,12 +1,14 @@
 function calcss() {
-    var sc = prompt("请输入收购价格:");
-    var prc = sc * 1.5;
-    alert("购买价格为:" + prc);
+  var sc = prompt("请输入收购价格:");
+  var prc = sc * 1.5;
+  alert("购买价格为:" + prc);
 }
 
-function seeerbaidu() {
+function seeerbaidu(question) {
+  if (question == undefined) {
     var question = prompt("请输入您的问题");
-    if (question == null) {} else {
-        location.replace("https://www.baidu.com/s?wd=" + question);
-    }
+  }
+  if (question != null) {
+    location.replace("https://www.baidu.com/s?wd=" + question);
+  }
 }
