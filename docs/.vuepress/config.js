733329f83373958333e45f1b1ab1b431914ea685
Yang_qwq <yang_qwq@qq.com>
2023-01-12 14:58:41 +0800

    更改文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,6 +12,13 @@ module.exports = {
     nav: [
       { text: "主页", link: "/" },
       { text: "指南", link: "/zh/" },
+      {
+        text: "插件教程",
+        items: [
+          { text: "皮肤插件", link: "/zh/skin/" },
+          { text: "粘液科技", link: "/zh/slimefun/" },
+        ],
+      },
       { text: "问问题", link: "/zh/questions/" },
       { text: "规则", link: "/zh/rules/" },
       { text: `定价`, link: "/zh/economic/" },
