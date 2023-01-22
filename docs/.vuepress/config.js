b2cc4d0515c4bdf6fa39f61584323d26966a09de
Yang <yang_qwq@qq.com>
2023-01-22 19:35:14 +0800

    init (#5)

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,6 +17,7 @@ module.exports = {
         items: [
           { text: "皮肤插件", link: "/zh/skin/" },
           { text: "粘液科技", link: "/zh/slimefun/" },
+          { text: "点歌插件", link: "/zh/allmusic/" },
         ],
       },
       { text: "问问题", link: "/zh/questions/" },
